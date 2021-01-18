5ca23e896275694f28914fa7714e43f92f14478d
John Morehouse <46388603+johnmorehouse@users.noreply.github.com>
2021-01-17 19:06:04 -0800

    Update check_output.sh

--- check_output.sh
+++ check_output.sh
@@ -1,6 +1,9 @@
 #This script checks for an "output file" in a bucket from a program being executed on GCE instance
-	#if the file exists, it stops the instance. 
+	#If the file exists, it stops the instance. 
 	#I usually run this with "cron" -- which executes this program every X minutes
+	#NOTE: you may need to give yourself permission to execute this (not as sudo). can use something like
+		#sudo chmod 755 '<PATH_TO>check_output.sh' in your terminal
+
 
 #get path from running echo $PATH in the terminal
 PATH=<PATH HERE>
